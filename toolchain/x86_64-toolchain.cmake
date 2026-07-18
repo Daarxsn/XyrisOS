47b1ca92c5ff4a4cd9ea6c2af2db5e6ceda19309
Joylin <devappajoylin@gmail.com>
2026-07-18 11:50:29 +0000

    fix: configure CMake try-compile for freestanding toolchain

--- toolchain/x86_64-toolchain.cmake
+++ toolchain/x86_64-toolchain.cmake
@@ -11,3 +11,5 @@ set(CMAKE_C_STANDARD 23)
 set(CMAKE_C_FLAGS "-ffreestanding -fno-stack-protector -fno-stack-check -fno-lto -fno-pic -fno-pie -ffunction-sections -fdata-sections -m64 -march=x86-64 -mabi=sysv -mno-80387 -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -mcmodel=kernel -Wall -Wextra")
 
 set(CMAKE_EXE_LINKER_FLAGS "-nostdlib -static")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
